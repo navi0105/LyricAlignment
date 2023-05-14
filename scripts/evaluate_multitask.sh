b787189bfe95dde0086d98450e40fa3299fe3f57
navi0105 <ivan000105@gmail.com>
2023-05-14 16:14:39 +0800

    (in progress) ctc branch first commit

--- scripts/evaluate_multitask.sh
+++ scripts/evaluate_multitask.sh
@@ -4,27 +4,7 @@ multitask_dev=${2}
 transcript_train=${3}
 transcript_dev=${4}
 
-whipser_model=${5}
-
-multitask_model_dir=${6}
-
-mkdir -p ${multitask_model_dir}
-cp ${0} ${multitask_model_dir}
-
-# Train multitask
-python train_multitask.py \
-    --train-data ${multitask_train} \
-    --dev-data ${multitask_dev} \
-    --whisper-model ${whipser_model} \
-    --device cuda \
-    --train-batch-size 2 \
-    --dev-batch-size 8 \
-    --accum-grad-steps 8 \
-    --lr 5e-4 \
-    --train-steps 2000 \
-    --eval-steps 100 \
-    --warmup-steps 200 \
-    --save-dir ${multitask_model_dir}
+multitask_model_dir=${5}
 
 # Inference Transcript
 result_1=${multitask_model_dir}/result_opencpop_test.json
